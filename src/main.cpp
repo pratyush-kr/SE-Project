35c2a8e7bdd2224def07fa22c0276125e7ea54d8
pratyush-kr <pratyush151198@gmail.com>
2021-08-08 20:01:53 +0530

    LoL

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,13 @@
 int main()
 {
     Library myLib("General Lib");
+    myLib.AddBook();
+    printf("\n");
+    myLib.AddMember();
+    printf("THE BOOK\n");
+    myLib.ShowBook();
+    printf("THE MEMBER\n");
+    myLib.ShowMember();
+
     return 0;
 }
